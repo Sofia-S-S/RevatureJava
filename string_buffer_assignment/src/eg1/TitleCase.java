521ac5736f00511674f1d65ec609b094c4938db2
Sofka <sofka.vs@gmail.com>
2020-12-20 23:40:18 -0600

    completed: palindrome, toUppercase, substring

--- string_buffer_assignment/src/eg1/TitleCase.java
+++ string_buffer_assignment/src/eg1/TitleCase.java
@@ -4,21 +4,19 @@ public class TitleCase {
 
 	public static void main(String[] args) {
 		
-		String s="hello hi good evening how are you doing today";
-		StringBuilder sb=new StringBuilder();
-		String ar[]=s.split(" ");
-		
-		//Convert every word's first character to uppercase and print back the sentence.
-		
-		for (int i = 0; i < ar.length; i++) {
-			sb.append(Character.toUpperCase(ar[i].charAt(0))).append(ar[i].substring(1)).append(" ");
-		}
-		System.out.println(sb.toString().trim());
-		
-		
+//		String s="hello hi good evening how are you doing today";
+//		StringBuilder sb=new StringBuilder();
+//		String ar[]=s.split(" ");
+//		for (int i = 0; i < ar.length; i++) {
+//			sb.append(Character.toUpperCase(ar[i].charAt(0))).append(ar[i].substring(1)).append(" ");
+//		}
+//		System.out.println(sb.toString().trim());
 		
 		//Task - Convert every word's last character to uppercase and print back the sentence.
 		
+		String s="hello hi good evening how are you doing today";
+		StringBuilder sb=new StringBuilder();
+		String ar[]=s.split(" ");
 		for (int i = 0; i < ar.length; i++) {
 			sb.append(ar[i].substring(0,ar[i].length()-1)).append(Character.toUpperCase(ar[i].charAt(ar[i].length()-1))).append(" ");
 		}
